6e021e700b20ff64c2cebb4dfa7d451baee8f1a4
Joshua <joshua.vivero.e@gmail.com>
2026-09-22 14:10:56 -0500

    Anadi la opcion al builder de wifi

---
+++ laboratorios/semana-06/builder.py
@@ -0,0 +1,49 @@
+class Computadora:
+    def __init__(self):
+        self.cpu=None
+        self.disco= None
+        self.ram =None
+        self.gpu = None
+        self.wifi=None
+    
+
+    def mostrar(self):
+        print('CPU: ' , self.cpu)
+        print('RAM: ' , self.ram)
+        print('GPU: ' , self.gpu)
+        print('DISCO: ', self.disco)
+        print('Wifi:', self.wifi)
+
+
+class ComputadoraBuilder:
+    def __init__(self):
+        self.computadora = Computadora()
+    def addCPU(self, cpu):
+        self.computadora.cpu = cpu
+        return self
+    def addDisco(self, disco):
+        self.computadora.disco=disco
+        return self
+    def addRam(self, ram):
+        self.computadora.ram=ram
+        return self
+    def addGPU(self, gpu):
+        self.computadora.gpu=gpu
+        return self
+    def addWifi(self, wifi):
+        self.computadora.wifi=wifi
+        return self
+    def build(self):
+        return self.computadora
+
+def main():
+    pc_builder= ComputadoraBuilder()
+
+    pc_builder= pc_builder.addRam(4).addGPU(18)
+
+    pc_gaming = pc_builder.addDisco(1).addCPU(20).addWifi('5G').build()
+
+    pc_gaming.mostrar()
+
+main()
+
